eb61596b0389aaf417d16d8bb156a201a817e2cf
Chevalinfotech24 <chevalinfotech@gmail.com>
2024-09-03 01:01:35 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
-var video = $('.card--recipe').hover(hoverVideo, hideVideo);
+$(document).ready(function() {
+    var cardWidth = $('.card--recipe').outerWidth(true);
+    var currentIndex = 0;
+    var cardCount = $('.card--recipe').length;
 
-function hoverVideo(e) {
-    $('video', this).get(0).play();
-    $('.card--recipe').addClass('is--playing');
-}
+    function showCard(index) {
+        var offset = -index * cardWidth;
+        $('.card-wrapper').css('transform', 'translateX(' + offset + 'px)');
+    }
 
-function hideVideo(e) {
-    $('video', this).get(0).pause();
-    $('.card--recipe').removeClass('is--playing');
-}
+    $('.next-button').click(function() {
+        if (currentIndex < cardCount - 1) {
+            currentIndex++;
+            showCard(currentIndex);
+        }
+    });
+
+    $('.prev-button').click(function() {
+        if (currentIndex > 0) {
+            currentIndex--;
+            showCard(currentIndex);
+        }
+    });
+
+    // Handle video play/pause on hover
+    $('.card--recipe').hover(
+        function() {
+            $('video', this).get(0).play();
+            $(this).addClass('is--playing');
+        },
+        function() {
+            $('video', this).get(0).pause();
+            $(this).removeClass('is--playing');
+        }
+    );
+});
